f38a0d81ef3261ca38fcb1ceb45562fe4e0995a1
nbogie <neillbogie@googlemail.com>
2021-03-09 23:02:42 +0000

    add comment explaining the minimal db connection config

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,4 +1,8 @@
 import { question } from "readline-sync";
 import { Client } from "pg";
+
+//As your database is on your local machine, with default port,
+//and default username and password,
+//we only need to specify the (non-default) database name.
 const client = new Client({ database: 'omdb' });
 console.log("Welcome to search-movies-cli!");
