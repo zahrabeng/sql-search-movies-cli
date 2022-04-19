a06c4e7fc684f3c06b7d38b2307e5953f63ec444
Zahra <bghzahra@outlook.com>
2022-04-19 17:31:50 +0100

    added while loop to loop through the CLI and keep asking the question until a user decides to quit

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -6,26 +6,29 @@ const readlineSync = require('readline-sync');
 //and default username and password,
 //we only need to specify the (non-default) database name.
 const client = new Client({ database: 'omdb' });
+
 console.log("Welcome to search-movies-cli!");
-
-
-const searchString = readlineSync.question("Search for what movie? ('q' to quit): ")
-const lowerCaseInput = searchString.toLowerCase()
-
 
 async function excecute (){
     await client.connect();
-    const text = "SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies WHERE kind = $1 AND LOWER(name) LIKE $2 ORDER BY date LIMIT 5";
-    const value = ["movie", `%${lowerCaseInput}%`];
 
-    if(searchString === 'q'){
+    let searchString = readlineSync.question("Search for what movie? ('q' to quit): ")
+    let lowerCaseInput = searchString.toLowerCase()
+
+    while(lowerCaseInput !== 'q'){
+        const text = "SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies WHERE kind = $1 AND LOWER(name) LIKE $2 ORDER BY date LIMIT 5";
+        const value = ["movie", `%${lowerCaseInput}%`];
+    
+        const res = await client.query(text,value);
+        console.table(res.rows);
+        lowerCaseInput = readlineSync.question("Search for what movie? ('q' to quit): ")
+    }
+        
+    if(lowerCaseInput === 'q'){
         console.log('Quit Successfully')
-        await client.end();
+        await client.end(); 
     }
 
-    const res = await client.query(text,value);
-    console.table(res.rows);
-    await client.end();
 }
 
 excecute()
