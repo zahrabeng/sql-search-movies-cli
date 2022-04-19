652d8f3efae0e5b033f691a1db4029d73f914215
Zahra <bghzahra@outlook.com>
2022-04-19 15:46:22 +0100

    added readlinesync question variable

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,5 +1,6 @@
 import { question } from "readline-sync";
 import { Client } from "pg";
+const readlineSync = require('readline-sync');
 
 //As your database is on your local machine, with default port,
 //and default username and password,
@@ -7,13 +8,16 @@ import { Client } from "pg";
 const client = new Client({ database: 'omdb' });
 console.log("Welcome to search-movies-cli!");
 
+
+const searchString = readlineSync.question("Search for what movie?")
+
 async function excecute (){
     await client.connect();
-    const text = "SELECT * FROM movies WHERE kind = $1 LIMIT 5";
-    const value = ["movie"];
+    const text = "SELECT * FROM movies WHERE kind = $1 AND name LIKE $2 LIMIT 5";
+    const value = ["movie" , ];
 
     const res = await client.query(text,value);
-    console.log(res.rows);
+    console.table(res.rows);
     await client.end();
 }
 
